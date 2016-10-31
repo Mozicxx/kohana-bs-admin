b9f11ccb966469c8bbf334631eb9486233725d2f
Mozic <wpyang_code@126.com>
2016-10-31 18:30:42 +0800

    statistics modified

--- modules/backend/classes/Controller/Backend/Statistics.php
+++ modules/backend/classes/Controller/Backend/Statistics.php
@@ -5,7 +5,6 @@ defined('SYSPATH') or die('No direct Script access!');
 class Controller_Backend_Statistics extends Controller_Backend
 {
     public function action_app(){
-        
     }
 
     public function action_page(){
